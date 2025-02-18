621f970f7327275690aae9d66bed55a1b6253e72
inmare <yatsumehole@naver.com>
2025-02-18 17:11:02 +0900

    router 추가

--- frontend/src/components/router.tsx
+++ frontend/src/components/router.tsx
@@ -1,15 +1,31 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Layout from './Layout.tsx';
 import App from './App.tsx';
 import Category from './Category.tsx';
+import CategoryTitle from './CategoryTitle.tsx';
+import Song from './Song.tsx';
 
 const router = createBrowserRouter([
 	{
-		path: '/',
-		element: <App />,
-	},
-	{
-		path: '/title/:titleId',
-		element: <Category />,
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <App />,
+			},
+			{
+				path: '/title',
+				element: <Category />,
+			},
+			{
+				path: '/title/:titleId',
+				element: <CategoryTitle />,
+			},
+			{
+				path: '/song/:songTitle',
+				element: <Song />,
+			},
+		],
 	},
 ]);
 
