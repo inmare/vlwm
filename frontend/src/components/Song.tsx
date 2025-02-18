24b43b51b341aa9634c673f0afd2ff53c64fe7d1
inmare <inmare2000@proton.me>
2025-02-18 22:34:52 +0900

    vocadb에서 영상 출처를 통해서 노래를 받는 코드 추가

--- frontend/src/components/Song.tsx
+++ frontend/src/components/Song.tsx
@@ -5,6 +5,7 @@ import Lyrics from './Lyrics';
 
 export default function CategoryTitle() {
 	const [data, setData] = useState([]);
+	const [player, setPlayer] = useState('');
 	const {songTitle} = useParams();
 
 	useEffect(() => {
@@ -14,13 +15,32 @@ export default function CategoryTitle() {
 	async function getSong() {
 		const response = await fetch(`/api/song/${songTitle}`);
 		const song = await response.json();
-		// console.log(song.songs);
 		setData(song);
+		// console.log(song.songs);
+		const originalUrl = song.songs[0].originalUrl;
+		const url = new URL(originalUrl);
+		let id;
+		for (const value of url.searchParams.values()) {
+			id = value;
+		}
+
+		const query = `byPv?pvService=Youtube&pvId=${id}&fields=PVs`;
+		const vocadbResponse = await fetch('/vocadb/' + query);
+		const vocadbResult = await vocadbResponse.json();
+
+		for (const pv of vocadbResult.pvs) {
+			if (pv.service == 'Youtube' && pv.pvType == 'Original') {
+				const pvId = pv.pvId;
+				setPlayer(`https://www.youtube.com/embed/${pvId}?feature=oembed`);
+				break;
+			}
+		}
 	}
 
 	return (
 		<>
 			<h1>{songTitle}</h1>
+			<iframe src={player} allowFullScreen={true} frameborder='0'></iframe>
 			<InfoTable songs={data.songs}/>
 			<Lyrics lyrics={data.lyrics}/>
 		</>
