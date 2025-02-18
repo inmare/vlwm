be2a783d490921b6c84ce9c8df19b8c639f23d53
inmare <inmare2000@proton.me>
2025-02-18 22:32:59 +0900

    백엔드에서 처리하는 로직 삭제

--- frontend/src/components/InfoTable.tsx
+++ frontend/src/components/InfoTable.tsx
@@ -3,20 +3,22 @@ export default function InfoTable({songs}: {songs: any}) {
 		return <p>Loading</p>;
 	}
 
-	return <>
-		<table>
-			<tbody>
-				{Object.entries(songs[0]).map(([key, value]: any, index: number) => {
-					if (key !== 'id' && key !== 'page_id' && value !== null) {
-						return (
-							<tr key={index}>
-								<th>{key}</th>
-								<td>{value}</td>
-							</tr>
-						);
-					}
-				})}
-			</tbody>
-		</table>
-	</>;
+	return (
+		<>
+			<table>
+				<tbody>
+					{Object.entries(songs[0]).map(([key, value]: any, index: number) => {
+						if (value !== null) {
+							return (
+								<tr key={index}>
+									<th>{key}</th>
+									<td>{value}</td>
+								</tr>
+							);
+						}
+					})}
+				</tbody>
+			</table>
+		</>
+	);
 }
