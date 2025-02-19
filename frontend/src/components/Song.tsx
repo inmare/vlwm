e3b070d8fd6f4a07753f16146abd4748cf8033a5
inmare <inmare2000@proton.me>
2025-02-19 22:54:19 +0900

    데이터 타입 및 플레이어 링크 추가

--- frontend/src/components/Song.tsx
+++ frontend/src/components/Song.tsx
@@ -1,10 +1,11 @@
 import {useState, useEffect} from 'react';
 import {useParams, Link} from 'react-router-dom';
+import {type DbResult} from '@ts/dbType';
 import InfoTable from './InfoTable';
 import Lyrics from './Lyrics';
 
 export default function CategoryTitle() {
-	const [data, setData] = useState([]);
+	const [data, setData] = useState<DbResult | undefined>();
 	const [player, setPlayer] = useState('');
 	const {songTitle} = useParams();
 
@@ -16,33 +17,30 @@ export default function CategoryTitle() {
 		const response = await fetch(`/api/song/${songTitle}`);
 		const song = await response.json();
 		setData(song);
-		// console.log(song.songs);
 		const originalUrl = song.songs[0].originalUrl;
 		const url = new URL(originalUrl);
-		let id;
-		for (const value of url.searchParams.values()) {
-			id = value;
+		let link;
+		if (url.hostname.includes('youtube')) {
+			const id = url.searchParams.get('v');
+			link = `https://www.youtube.com/embed/${id}`;
+		} else if (url.hostname.includes('nicovideo')) {
+			link = `https://embed.nicovideo.jp${url.pathname}`;
 		}
 
-		const query = `byPv?pvService=Youtube&pvId=${id}&fields=PVs`;
-		const vocadbResponse = await fetch('/vocadb/' + query);
-		const vocadbResult = await vocadbResponse.json();
-
-		for (const pv of vocadbResult.pvs) {
-			if (pv.service == 'Youtube' && pv.pvType == 'Original') {
-				const pvId = pv.pvId;
-				setPlayer(`https://www.youtube.com/embed/${pvId}?feature=oembed`);
-				break;
-			}
-		}
+		setPlayer(link);
+		// TODO: 현재 니코동 플레이어는 재생이 안되는 에러가 있는데 이게 단순히 test 서버라서 그런지 체크 필요
 	}
 
 	return (
 		<>
-			<h1>{songTitle}</h1>
-			<iframe src={player} allowFullScreen={true} frameborder='0'></iframe>
-			<InfoTable songs={data.songs}/>
-			<Lyrics lyrics={data.lyrics}/>
+			<h1>{data?.page.pageTitle}</h1>
+			<p>{data?.page.originalTitle}</p>
+			<iframe src={player} allow='autoplay; encrypted-media;' allowFullScreen='allowfullscreen' frameBorder='0' scrolling='no'></iframe>
+			<p>
+				<a href={'http://vocaro.wikidot.com/' + data?.page.pageUrl} target='_blank'>원본 링크</a>
+			</p>
+			<InfoTable songs={data?.songs}/>
+			<Lyrics lyrics={data?.lyrics}/>
 		</>
 	);
 }
