39466c456b5e7590011d8af0df73b5327a0f6fbc
inmare <inmare2000@proton.me>
2025-03-10 22:46:07 +0900

    category를 div로 정렬하도록 수정

--- frontend/src/components/Category.tsx
+++ frontend/src/components/Category.tsx
@@ -1,13 +1,32 @@
-import {titleList} from '@ts/titleType';
+import {useEffect, useState} from 'react';
+import '@scss/Category.scss';
 
 export default function Category() {
-	return (<>
-		<ul>
-			{titleList.map((title, index) => (
-				<li key={index}>
-					<a href={'/title' + `/${title.link}`}>{title.title}</a>
-				</li>
-			))}
-		</ul>
-	</>);
+	const [category, setCategory] = useState<string[]>([]);
+
+	useEffect(() => {
+		getTitle();
+	}, []);
+
+	async function getTitle() {
+		const response = await fetch('api/title');
+		const title = await response.json();
+		setCategory(title);
+	}
+
+	return (
+		<>
+			<h1>분류:제목</h1>
+			<div className='category-wrapper'>
+				{
+					category.map((value: string, index) => (
+						<div key={index} className='card-wrapper'>
+							<div className='category-card'><a href={`title/${value}`}>{value}</a></div>
+						</div>
+					))
+				}
+			</div>
+
+		</>
+	);
 }
