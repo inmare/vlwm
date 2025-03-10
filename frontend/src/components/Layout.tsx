0aed357f2e9030fdd9e19d5bceecbaefc724c577
inmare <inmare2000@proton.me>
2025-03-10 22:47:17 +0900

    모바일 화면 대응용 화면 크기 설정 코드 추가

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,31 @@
 import {Outlet} from 'react-router-dom';
 import Header from './Header';
+import '@scss/main.scss';
+import {useState, useEffect} from 'react';
 
 export default function Layout() {
+	const [height, setHeight] = useState(0);
+
+	useEffect(() => {
+		const updateHeight = () => {
+			setHeight(globalThis.innerHeight);
+		};
+
+		window.addEventListener('resize', updateHeight);
+
+		updateHeight();
+	}, []);
+
 	return (
 		<>
-			<Header/>
-			<Outlet/>
+			<div style={{height: `${height - 100}px`}}>
+				<Header/>
+				<main >
+					<div className='wrapper'>
+						<Outlet/>
+					</div>
+				</main>
+			</div>
 		</>
 	);
 }
