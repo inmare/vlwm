d66ab4dbbac55e1576815934d3707c2df32ae914
inmare <inmare2000@proton.me>
2025-03-10 22:46:23 +0900

    App 대신 Home으로 컴포넌트명 변경

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,13 +1,12 @@
-import '@scss/App.scss';
 import {Link} from 'react-router-dom';
 
-export default function App() {
+export default function Home() {
 	return (
 		<>
 			<h1>분류 보기</h1>
 			<ul>
 				<li>
-					<Link to='/title'>한글 자모</Link>
+					<Link to='/title'>제목</Link>
 				</li>
 			</ul>
 		</>
