30fdfa4ec348f0cd54b10f8530d257fcb03084b6
inmare <yatsumehole@naver.com>
2025-03-11 15:13:08 +0900

    card 정렬 함수 추가

--- frontend/src/components/Category.tsx
+++ frontend/src/components/Category.tsx
@@ -1,11 +1,24 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import '@scss/Category.scss';
 
 export default function Category() {
 	const [category, setCategory] = useState<string[]>([]);
+	const [isNarrow, setNarrow] = useState(false);
 
 	useEffect(() => {
+		const updateIsNarrow = () => {
+			const narrow = (window.innerWidth > 700);
+			setNarrow(narrow);
+		};
+
+		window.addEventListener('resize', updateIsNarrow);
+		updateIsNarrow();
+
 		getTitle();
+
+		return () => {
+			window.removeEventListener('resize', updateIsNarrow);
+		};
 	}, []);
 
 	async function getTitle() {
@@ -14,19 +27,42 @@ export default function Category() {
 		setCategory(title);
 	}
 
+	const arrangeCategory = useMemo(() => {
+		const sliceAmount = isNarrow ? 4 : 2;
+		const cardRow = [];
+
+		for (let i = 0; i < category.length; i += sliceAmount) {
+			const slicedCategory = category.slice(i, i + sliceAmount);
+			const cards = (
+				<div key={i} className='card-row' style={{zIndex: Math.trunc(i / sliceAmount)}}>
+					{
+						slicedCategory.map((value, index) => (
+							<div key={i + index} className='card-wrapper'>
+								<a href={`title/${value}`}>
+									<div className='card'>
+										{value}
+									</div>
+								</a>
+							</div>
+						))
+					}
+				</div>
+			);
+
+			cardRow.push(cards);
+		}
+
+		return cardRow;
+	}, [category, isNarrow]);
+
 	return (
 		<>
 			<h1>분류:제목</h1>
+			<hr />
 			<div className='category-wrapper'>
-				{
-					category.map((value: string, index) => (
-						<div key={index} className='card-wrapper'>
-							<div className='category-card'><a href={`title/${value}`}>{value}</a></div>
-						</div>
-					))
-				}
+				{arrangeCategory}
 			</div>
-
+			<div className='row-end'></div>
 		</>
 	);
 }
